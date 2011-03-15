11c56ba60302e88cdf0763b8d1678e931ba7340a
mikko@redinnovation.com <mikko@redinnovation.com@985c90ce-6bbf-11de-a1e0-47924000d34a>
2011-03-15 11:47:43 +0000

    Prepare mfabrik.webandmobile 1.0.2.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.1'
+version = '1.0.2'
 
 setup(name='mfabrik.webandmobile',
       version=version,
@@ -33,7 +33,7 @@ setup(name='mfabrik.webandmobile',
           'gomobile.mobile==1.0.0',
           'gomobile.convergence==1.0.0',
           'gomobile.supporter==0.9',
-          'gomobiletheme.basic==1.0.0',
+          'gomobiletheme.basic==1.0.2',
           'gomobile.imageinfo==0.9.1',
           # -*- Extra requirements: -*-
       ],
